649b3c1668cf92f6a50e09363845fa081c0b1930
yamit <yamit.cardenas@gmail.com>
2021-10-29 12:05:54 -0500

    first test, the rover got a great setup

--- spec/rover-spec.ts
+++ spec/rover-spec.ts
@@ -2,12 +2,14 @@ import { Rover } from '../src/rover';
 describe("Mars Rover", ()=> {
     let rover: Rover;
 
-    beforeEach(() => {
-        rover = new Rover();
-    });
-
-    it('Should be a Rover', () => {
-        expect(rover.constructor.name).toEqual("Rover");
+    it('Should start with map, position and orders', () => {
+        rover = new Rover("5 3", "1 1 E", "RFRFRFRF")
+        expect(rover.maxMarsDimensionX).toEqual(5);
+        expect(rover.maxMarsDimensionY).toEqual(3);
+        expect(rover.posX).toEqual(1);
+        expect(rover.posY).toEqual(1);
+        expect(rover.direction).toEqual("E");
+        expect(rover.orders).toEqual("RFRFRFRF");
     });
 
 });
--- src/rover.ts
+++ src/rover.ts
@@ -1,6 +1,22 @@
 export class Rover{
-    constructor(){
+    orders: string;
 
+    maxMarsDimensionX: number
+    maxMarsDimensionY: number
+    direction: string;
+    posX: number;
+    posY: number;
+
+
+    constructor(marsDimensions: string, initPosition: string, orders: string){
+        marsDimensions = marsDimensions.replace(/\s/g, "")
+        initPosition = initPosition.replace(/\s/g, "")
+        this.maxMarsDimensionX = Number(marsDimensions[0])
+        this.maxMarsDimensionY = Number(marsDimensions[1])
+        this.posX = Number(initPosition[0])
+        this.posY = Number(initPosition[1])
+        this.direction = initPosition[2]
+        this.orders = orders
     }
 
 }
