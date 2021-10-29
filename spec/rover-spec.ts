716df3a5040ae593c601ee5dd9cc634a955f8d83
yamit <yamit.cardenas@gmail.com>
2021-10-29 13:21:12 -0500

    the rover started to learn how to turn L or R

--- spec/rover-spec.ts
+++ spec/rover-spec.ts
@@ -1,15 +1,48 @@
 import { Rover } from '../src/rover';
 describe("Mars Rover", ()=> {
-    let rover: Rover;
+    let rover: Rover
 
     it('Should start with map, position and orders', () => {
         rover = new Rover("5 3", "1 1 E", "RFRFRFRF")
-        expect(rover.maxMarsDimensionX).toEqual(5);
-        expect(rover.maxMarsDimensionY).toEqual(3);
-        expect(rover.posX).toEqual(1);
-        expect(rover.posY).toEqual(1);
-        expect(rover.direction).toEqual("E");
-        expect(rover.orders).toEqual("RFRFRFRF");
+        expect(rover.maxMarsDimensionX).toEqual(5)
+        expect(rover.maxMarsDimensionY).toEqual(3)
+        expect(rover.posX).toEqual(1)
+        expect(rover.posY).toEqual(1)
+        expect(rover.direction).toEqual("E")
+        expect(rover.orders).toEqual("RFRFRFRF")
     });
+
+    it('Should change directions with R', () => {
+        rover = new Rover("1 1", "0 0 N", "")
+        expect(rover.direction).toEqual("N")
+        rover.move("R")
+        expect(rover.direction).toEqual("E")
+        rover.move("R")
+        expect(rover.direction).toEqual("S")
+        rover.move("R")
+        expect(rover.direction).toEqual("W")
+        rover.move("R")
+        expect(rover.direction).toEqual("N");
+    });
+
+    it('Should change directions with L', () => {
+        rover = new Rover("1 1", "0 0 N", "")
+        expect(rover.direction).toEqual("N")
+        rover.move("L")
+        expect(rover.direction).toEqual("W")
+        rover.move("L")
+        expect(rover.direction).toEqual("S")
+        rover.move("L")
+        expect(rover.direction).toEqual("E")
+        rover.move("L")
+        expect(rover.direction).toEqual("N");
+    });
+
+    it('Should execute orders and finish in N direction', () => {
+        rover = new Rover("1 1", "0 0 N", "LLRR")
+        rover.executeOrders()
+        expect(rover.direction).toEqual("N");
+    });
+
 
 });
--- src/rover.ts
+++ src/rover.ts
@@ -3,10 +3,13 @@ export class Rover{
 
     maxMarsDimensionX: number
     maxMarsDimensionY: number
-    direction: string;
+    direction: string
+
     posX: number;
     posY: number;
 
+    private directionIndex: number
+    private COORDINATES = ['N','E','S','W']
 
     constructor(marsDimensions: string, initPosition: string, orders: string){
         marsDimensions = marsDimensions.replace(/\s/g, "")
@@ -16,7 +19,40 @@ export class Rover{
         this.posX = Number(initPosition[0])
         this.posY = Number(initPosition[1])
         this.direction = initPosition[2]
+        this.directionIndex = this.COORDINATES.indexOf(this.direction)
         this.orders = orders
     }
+
+    executeOrders() {
+        for (let i = 0; i< this.orders.length; i++){
+            this.move(this.orders[i])
+        }
+    }
+
+    move(instruction: string) {
+        if(instruction === "R"){
+            this.turnRight();
+        }
+        if(instruction === "L"){
+            this.turnLeft();
+        }
+    }
+
+    private turnLeft() {
+        this.directionIndex--
+        if (this.directionIndex < 0) {
+            this.directionIndex = this.COORDINATES.length - 1
+        }
+        this.direction = this.COORDINATES[this.directionIndex]
+    }
+
+    private turnRight() {
+        this.directionIndex++
+        if (this.directionIndex == this.COORDINATES.length) {
+            this.directionIndex = 0
+        }
+        this.direction = this.COORDINATES[this.directionIndex]
+    }
+
 
 }
